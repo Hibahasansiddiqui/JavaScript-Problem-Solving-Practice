32dbba06e5b024b0512ae00dd309f1b366c738ee
Hibahasansiddiqui <hiba.hasan.siddiqui@gmail.com>
2021-05-28 16:49:11 +0500

    Operators problems solved with complete values

---
+++ Topic3 Operators/Operators Problem 1/app.js
@@ -0,0 +1,33 @@
+/*var x = 5, y = 10, z = 15*/
+
+let x = 5;
+let y = 10;
+let z = 15;
+
+/*x + y  -- to add*/
+let Sum = (x + y);
+console.log(Sum); 
+
+/* y - z -- for difference*/
+let minus = (y - z);
+console.log(minus);
+
+/*x * y  -- to multiply*/
+let multiply = (x * y);
+console.log(multiply);
+
+/*y / x-- to divide*/
+let divide = (y / x);
+console.log(divide);
+
+/*x % 2 -- for reminder/percentage*/
+let percentage = (x % 2);
+console.log(percentage);
+
+/* x ++  -- for increment*/
+let inctr = (x++);
+console.log(inctr);
+
+/*x--   -- for decrement*/
+let decre = (x--);
+console.log(decre);
