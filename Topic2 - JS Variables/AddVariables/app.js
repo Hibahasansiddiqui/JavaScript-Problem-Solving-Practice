9116947eaa57aa7c3f9fc19b65b0c0e2a1732321
Hibahasansiddiqui <hiba.hasan.siddiqui@gmail.com>
2021-05-28 15:46:34 +0500

    Added Variables finished values

---
+++ Topic2 - JS Variables/AddVariables/app.js
@@ -0,0 +1,11 @@
+/*Taking two numbers 4 and 12 and then
+initialize them by using "let" */
+
+let Numb1 = 4;
+let Numb2 = 12;
+
+/*Added both of them and Saved them as a new variable*/
+let Sum = (Numb1 + Numb2);
+
+/*Then print using console.log*/
+console.log(Sum);
