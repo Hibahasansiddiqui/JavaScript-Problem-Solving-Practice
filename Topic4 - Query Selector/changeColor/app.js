f7fe155a2c54ce5d56005b384833b88dc9b5d9d4
Hibahasansiddiqui <hiba.hasan.siddiqui@gmail.com>
2021-05-31 19:51:05 +0500

    Query selector finished html and js values

---
+++ Topic4 - Query Selector/changeColor/app.js
@@ -0,0 +1,4 @@
+// Function to change paragraph background color
+function changeParagraphBg(color){
+    document.getElementById("Paragraph").style.background = color;
+}
---
+++ Topic4 - Query Selector/changeFirst/app.js
@@ -0,0 +1,2 @@
+  // change the h1 text color
+  document.querySelector("div").style.color = "red";
