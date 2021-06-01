48a83709666074d901630fe165acbf63680d03a3
Hibahasansiddiqui <hiba.hasan.siddiqui@gmail.com>
2021-06-01 22:13:13 +0500

    datatype finished js values

---
+++ Topic6 - Datatypes/app.js
@@ -0,0 +1,16 @@
+let num1 = 27; //integer value
+let num2 = 27.25;//decimal value
+let str = string;// string value
+let bool = true;//boolean value
+let emp = null;//empty/null value
+
+//represents the print version of integer value to the variable
+console.log(typeof(num1));
+//represents the print version of decimal value to the variable
+console.log(typeof(num2));
+//represents the print version of string value to the variable
+console.log(typeof(str));
+//represents the print version of boolean value to the variable
+console.log(typeof(bool));
+//represents the print version of null value to the variable
+console.log(typeof(emp));
