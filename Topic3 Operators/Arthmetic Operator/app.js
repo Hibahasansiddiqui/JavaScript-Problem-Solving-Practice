df3055b0b2a95593b2aa74934628fc0d74be22b4
Hibahasansiddiqui <hiba.hasan.siddiqui@gmail.com>
2021-06-03 00:11:56 +0500

    JS Coding Task 2 Completed

---
+++ Topic3 Operators/Arthmetic Operator/app.js
@@ -0,0 +1,12 @@
+let var1 = 50 ;
+let var2 = 100;
+
+document.write("Sum/Addition" + (var1+var2));
+document.write("<br /> ");
+document.write("Multiplication" + (var1*var2));
+document.write("<br /> ");
+document.write("Division" + (var1/var2));
+
+alert("Sum/Addition" + (var1+var2));
+alert("Multiplication" + (var1*var2));
+alert("Division" + (var1/var2));
