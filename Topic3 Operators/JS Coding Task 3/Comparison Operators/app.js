6127e0721fb46242c1e1987fb64193232a728970
Hibahasansiddiqui <hiba.hasan.siddiqui@gmail.com>
2021-06-03 21:31:41 +0500

    JS Coding Task 3 Completed

---
+++ Topic3 Operators/JS Coding Task 3/Comparison Operators/app.js
@@ -0,0 +1,25 @@
+let x = 4
+let y = 5
+
+let check = x <= y; // This means it's going to check if 4 is less than or equal to 5
+                    //that is 4 <= 5 as mentioned in the question.
+
+console.log(check);
+
+//Comparision operators are:
+
+//== equal to
+
+// <= Checks whether left operand is less than or equal to right operand. 
+// If yes then returns true otherwise false.
+
+// >= Checks whether left operand is greater than or equal to right operand. 
+// If yes then returns true otherwise false.
+
+// !=	Compares inequality of two operands.
+
+// >	Checks whether left side value is greater than right side value. 
+// If yes then returns true otherwise false.
+
+// <	Checks whether left operand is less than right operand. 
+// If yes then returns true otherwise false.
---
+++ Topic3 Operators/JS Coding Task 3/Logical Operators/app.js
@@ -0,0 +1,33 @@
+//let m = True && True
+//console.log(m)
+
+//ReferenceError: False is not defined
+
+//let n = False && True
+//console.log(n)
+
+//ReferenceError: False is not defined
+
+//let x = False && False
+//console.log(x)
+
+//ReferenceError: False is not defined
+
+//let y = False || True
+//console.log(y)
+
+//ReferenceError: False is not defined
+
+let j = 5>4 && 2<5
+console.log(j)
+//Result: The statement is True
+
+let k = 2==9 || 2==2
+console.log(k)
+//Result: The statement is True
+
+let o = !(!(4>2))
+console.log(o)
+//Result: The statement is True
+
+
