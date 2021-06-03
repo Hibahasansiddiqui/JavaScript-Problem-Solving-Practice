490652a9f98acaeb5affcafad06bb3b3ed0492f1
Hibahasansiddiqui <hiba.hasan.siddiqui@gmail.com>
2021-06-03 23:02:56 +0500

    JS Coding Task 3 Completed

---
+++ Topic3 Operators/JS Coding Task 3/Logical Operators - Rocket Example/app.js
@@ -0,0 +1,9 @@
+//Take 3 variables
+
+let IsRain = false
+let IsSnow = false
+let IsSpeed = 15
+
+canLaunch = (IsRain == false) && (IsSnow == false) && (IsSpeed < 20)
+
+alert(canLaunch + "Rocket will be Launched")
