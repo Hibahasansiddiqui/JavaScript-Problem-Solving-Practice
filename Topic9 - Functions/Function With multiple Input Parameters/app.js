fe2b813a7c1f515a5a9001b8d32264d0cf9206dd
Hibahasansiddiqui <hiba.hasan.siddiqui@gmail.com>
2021-06-05 21:01:02 +0500

    JS Coding Task 5 Completed

---
+++ Topic9 - Functions/Function With multiple Input Parameters/app.js
@@ -0,0 +1,7 @@
+function AddTwoNumbers(Num1, Num2) {
+    var sum = Num1 + Num2;
+    console.log(sum)
+    return sum;
+}
+
+AddTwoNumbers(90,93)
---
+++ Topic9 - Functions/Introduction to Functions/app.js
@@ -0,0 +1,9 @@
+function Double(paisa) {
+    var answer = paisa *2;
+    console.log(answer);
+    return answer;
+}
+
+Double(10)
+Double(3)
+Double(5)
