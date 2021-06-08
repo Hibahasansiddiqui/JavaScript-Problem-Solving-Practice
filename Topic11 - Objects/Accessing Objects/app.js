1a5e1ee97aabae3f3889e1909b7def558b0abc9c
Hibahasansiddiqui <hiba.hasan.siddiqui@gmail.com>
2021-06-08 23:12:29 +0500

    JS Coding Task 8 Completed

---
+++ Topic11 - Objects/Accessing Objects/app.js
@@ -0,0 +1,10 @@
+var cities = {
+    first: "Karachi",
+    second: "Lahore",
+    third: "Islamabad",
+    fourth: "Quetta",
+ }
+//console.log('Karachi', 'Quetta')
+
+console.log(cities.first)
+console.log(cities.fourth)
---
+++ Topic11 - Objects/Objects/app.js
@@ -0,0 +1,12 @@
+//let cities = ["Karachi", "Lahore", "Islamabad", "Quetta"]
+//console.log(cities)
+
+//OR
+
+var cities = {
+   first: "Karachi",
+   second: "Lahore",
+   third: "Islamabad",
+   fourth: "Quetta"
+}
+console.log(cities)
