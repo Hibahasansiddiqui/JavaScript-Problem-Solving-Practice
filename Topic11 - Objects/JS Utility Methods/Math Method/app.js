1f51e23db349bfbd8f0fbf6eb524897039a10d49
Hibahasansiddiqui <hiba.hasan.siddiqui@gmail.com>
2021-06-08 23:49:46 +0500

    JS Coding Task 8 Completed

---
+++ Topic11 - Objects/JS Utility Methods/Math Method/app.js
@@ -0,0 +1,4 @@
+var height = 7.534564
+
+console.log(Math.floor(height)) //floor method
+console.log(Math.ceil(height))  //ceil method
---
+++ Topic11 - Objects/JS Utility Methods/String Method/app.js
@@ -0,0 +1,6 @@
+var cityName = "Tokyo"
+console.log(cityName.length) //to print length of the city name "Tokyo"
+
+console.log(cityName.indexOf("o")) //to print index of "o"
+
+console.log(cityName.search("o")) //print search method
