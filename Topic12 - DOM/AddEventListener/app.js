199d680e22ac619937de1ff284ce6b96df5c41a5
Hibahasansiddiqui <hiba.hasan.siddiqui@gmail.com>
2021-06-10 23:37:25 +0500

    JS Coding Task 9 Completed

---
+++ Topic12 - DOM/AddEventListener/app.js
@@ -0,0 +1,4 @@
+document.addEventListener("click", function(){
+    var x = document.querySelector('button');
+    x = alert("You just pushed a RED BUTTON, You're doomed.....");
+});
---
+++ Topic12 - DOM/QuerySelecter/app.js
@@ -0,0 +1,3 @@
+var first = document.querySelector('.div-class');
+
+first.classList.replace("div-class", 'changed-class');
